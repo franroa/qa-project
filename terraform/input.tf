3dd6805a01005cbe91f78adf5c542d372a1943fc
franroasynaos <francisco.roa.prieto@synaos.com>
2021-07-16 17:11:28 +0200

    fmt

--- terraform/input.tf
+++ terraform/input.tf
@@ -12,7 +12,7 @@ variable "resource_group" {}
 variable "application_type" {}
 
 # Network
-variable virtual_network_name {}
-variable address_prefix_test {}
-variable address_space {}
+variable "virtual_network_name" {}
+variable "address_prefix_test" {}
+variable "address_space" {}
 
